058ecfa9713e135ba944f0300b6c9fdc583014b6
HL <hli@theaiinstitute.com>
2024-08-15 16:32:38 -0400

    minor: change default settings

--- examples/convert_obj_to_stl.py
+++ examples/convert_obj_to_stl.py
@@ -27,10 +27,10 @@ def convert_obj_to_stl(obj_file, stl_file):
 if __name__ =="__main__":
     parser = argparse.ArgumentParser(description="Converting an obj file to an stl file")
     parser.add_argument(
-        "--obj_path", type=str, default="results/generated_terrain", help="Path to the obj file"
+        "--obj_path", type=str, default="results/primitive_separated/generate_complex_terrain/mesh_complex_terrain.obj", help="Path to the obj file"
     )
     parser.add_argument(
-        "--stl_path", type=str, default="results/generated_terrain/terrains", help="Path to the exported stl file"
+        "--stl_path", type=str, default="results/primitive_separated/generate_complex_terrain/mesh_complex_terrain.stl", help="Path to the exported stl file"
     )
     args = parser.parse_args()
     convert_obj_to_stl(args.obj_path, args.stl_path)
